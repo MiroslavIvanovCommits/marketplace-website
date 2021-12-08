4a45e824552ab04bb3d549c7cb0a5991bb6fd8a5
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-08 12:07:00 +0200

    Get initial template to work

--- src/App.js
+++ src/App.js
@@ -3,8 +3,505 @@
 function App() {
   return (
     <div>
-      <div id="container">
-        <p>React</p>
+
+      <div className="preloader">
+      <div className="loader theme_background_color">
+      <span></span>
+
+      </div>
+      </div>
+
+      <div className="wrapper">
+
+      <nav  className=" nim-menu navbar navbar-default navbar-fixed-top">
+      <div className="container">
+
+      <div className="navbar-header">
+      <button type ="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span className="sr-only">Toggle navigation</span>
+      <span className="icon-bar"></span>
+      <span className="icon-bar"></span>
+      <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="index.html">N<span className="themecolor">i</span>M</a>
+      </div>
+
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav navbar-right">
+      <li><a href="#home" className="page-scroll">Home</a></li>
+      <li><a href="#one" className="page-scroll">About</a></li>
+      <li><a href="#two" className="page-scroll">Our Works</a></li>
+      <li><a href="#three" className="page-scroll">Team</a></li>
+      <li><a href="#four" className="page-scroll">Inspiration</a></li>
+      <li><a href="#five" className="page-scroll">Status</a></li>
+      <li><a href="#six" className="page-scroll">Testimonials</a></li>
+      <li><a href="#seven" className="page-scroll">Services</a></li>
+      <li><a href="#eight" className="page-scroll">Contact</a></li>
+      </ul>
+      </div>
+      </div>
+    </nav>
+
+      <section className="main-heading" id="home">
+      <div className="overlay">
+      <div className="container">
+      <div className="row">
+      <div className="main-heading-content col-md-12 col-sm-12 text-center">
+      <h1 className="main-heading-title">We are Creative</h1>
+      <p className="main-heading-text">Et harum quidem rerum facilis est et expedita distinctio.Nam libero tempore,<br/>cum soluta nobis est eligendi optio cumque nihil impedit quo facilis</p>
+      <div className="btn-bar">
+      <a href="#" className="btn btn-custom theme_background_color">Ge Started</a>
+      <a href="#" className="btn btn-custom-outline">Contact Us</a>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      </section>
+
+      <section className="aboutus white-background black" id="one">
+      <div className="container">
+      <div className="row">
+      <div className="col-md-12 text-center black">
+      <h3 className="title">ABOUT <span className="themecolor">US</span></h3>
+      <p className="a-slog">Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty</p>
+      </div>
+      </div>
+      <div className="gap">
+
+      </div>
+
+      <div className="row about-box">
+      <div className="col-sm-4 text-center">
+      <div className="margin-bottom">
+      <i className="fa fa-newspaper-o"></i>
+      <h4>POWER OF FLEXIBILITY</h4>
+      <p className="black">Nulla vitae libero pharetra augue.Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+      </div>
+      </div>
+      <div className="col-sm-4 about-line text-center">
+      <div className="margin-bottom">
+      <i className="fa fa-diamond"></i>
+      <h4>FULLY RESPONSIVE</h4>
+      <p className="black">Nulla vitae libero pharetra augue.Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+      </div>
+      </div>
+          <div className="col-sm-4 text-center">
+      <div className="margin-bottom">
+      <i className="fa fa-area-chart"></i>
+      <h4>GREAT WEB IDEAS</h4>
+      <p className="black">Nulla vitae libero pharetra augue.Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+      </div>
+      </div>
+        </div>
+
+      </div>
+      </section>
+
+      <section className="recent-works text-center" id="two">
+      <div className="container">
+      <div className="row">
+      <div className="col-md-12">
+      <h3 className="title">Recent <span className="themecolor">Works</span></h3>
+      <p className="a-slog">Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty</p>
+      </div>
+      </div>
+
+      <div className="gap"></div>
+
+      <div className="row">
+      <div className="col-sm-4 port-item margin-bottom">
+      <div className="item-img-wrap">
+      <img src="images/work-1.jpg" className="img-responsive" alt="workimg" />
+      <div className="item-img-overlay">
+      <a href="images/work-1.jpg" className="show-image">
+      <span></span>
+      </a>
+      </div>
+      </div>
+      <div className="work-desc">
+      <h3><a href="#">Mockup Design</a></h3>
+      <span>Photoshop</span>
+      </div>
+      </div>
+
+      <div className="col-sm-4 port-item margin-bottom">
+      <div className="item-img-wrap">
+      <img src="images/work-2.jpg" className="img-responsive" alt="workimg" />
+      <div className="item-img-overlay">
+      <a href="images/work-2.jpg" className="show-image">
+      <span></span>
+      </a>
+      </div>
+      </div>
+      <div className="work-desc">
+      <h3><a href="#">Graphic Design</a></h3>
+      <span>Illustrator</span>
+      </div>
+      </div>
+
+      <div className="col-sm-4 port-item margin-bottom">
+      <div className="item-img-wrap">
+      <img src="images/work-3.jpg" className="img-responsive" alt="workimg" />
+      <div className="item-img-overlay">
+      <a href="images/work-3.jpg" className="show-image">
+      <span></span>
+      </a>
+      </div>
+      </div>
+      <div className="work-desc">
+      <h3><a href="#">Web Design</a></h3>
+      <span>Html / Css</span>
+      </div>
+      </div>
+      </div>
+
+      <div className="row">
+      <div className="col-md-12 text-center">
+      <a href="#" className="btn btn-custom theme_background_color">Load More</a>
+      </div>
+      </div>
+
+      </div>
+      </section>
+
+      <section className="our-team white-background black" id="three">
+      <div className="container">
+      <div className="row text-center">
+      <div className="col-md-12">
+      <h3 className="title">Creative <span className="themecolor">Team</span></h3>
+      <p className="a-slog">Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty</p>
+      </div>
+      </div>
+      <div className="row text-center">
+      <div className="col-sm-3">
+      <img src="images/team-1.jpg" alt="team-member" className="img-responsive" />
+      <h4>Ramshad Smith</h4>
+      <h5>Founder</h5>
+      <p>Hey, I’m Johm Smith residing in this beautiful world.I create websites and mobile...</p>
+      <ul className="list-inline top20">
+      <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+      <li><a href="#"><i className="fa fa-behance"></i></a></li>
+      </ul>
+      </div>
+
+      <div className="col-sm-3">
+      <img src="images/team-2.jpg" alt="team-member" className="img-responsive" />
+      <h4>Meera Deo</h4>
+      <h5>Web Designer</h5>
+      <p>Hey, I’m Meera Deo residing in this beautiful world.I create websites and mobile...</p>
+      <ul className="list-inline top20">
+      <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+      <li><a href="#"><i className="fa fa-behance"></i></a></li>
+      </ul>
+      </div>
+
+      <div className="col-sm-3">
+      <img src="images/team-3.jpg" alt="team-member" className="img-responsive" />
+      <h4>Ramkumar Deo</h4>
+      <h5>Manager</h5>
+      <p>Hey, I’m Ramkumar Deo residing in this beautiful world.I create websites and mobile...</p>
+      <ul className="list-inline top20">
+      <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+      <li><a href="#"><i className="fa fa-behance"></i></a></li>
+      </ul>
+      </div>
+
+      <div className="col-sm-3">
+      <img src="images/team-4.jpg" alt="team-member" className="img-responsive" />
+      <h4>Antony Kovalsky</h4>
+      <h5>Graphic Designer</h5>
+      <p>Hey, I’m Antony Kovalsky residing in this beautiful world.I create websites and mobile...</p>
+      <ul className="list-inline top20">
+      <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+      <li><a href="#"><i className="fa fa-behance"></i></a></li>
+      </ul>
+      </div>
+      </div>
+
+      <div className="row margin-top">
+      <div className="col-md-12 text-center">
+      <a href="#" className="btn btn-custom theme_background_color ">We are Hiring</a>
+      </div>
+      </div>
+
+      </div>
+      </section>
+
+      <section className="inspiration" id="four">
+      <div className="overlay">
+      <div className="container">
+      <div className="row">
+      <article className="col-md-12 text-center">
+      <div className="intermediate-container">
+      <div className="subheading">
+      <h4>Are You Ready To <span className="themecolor">Enjoy?</span></h4>
+      </div>
+      <div className="heading">
+      <h2>inspire your customer here!</h2>
+      </div>
+      <div className="">
+      <a className="btn btn-custom-outline" href="#"><span>get started</span></a>
+      </div>
+      </div>
+      </article>
+      </div>
+      </div>
+      </div>
+      </section>
+
+      <section className="our-stats" id="five">
+      <div className="container">
+      <div className="row">
+      <div className="col-md-3 col-sm-6">
+      <div className="our-stats-box text-center">
+      <div className="our-stat-icon">
+      <span className="fa fa-ge"></span>
+      </div>
+      <div className="our-stat-info">
+      <span className="stats" data- from ="4763904" data-to="4764504" data-speed="5000"
+      data-refresh-interval="50"></span>
+
+      <h5>Total Downloaded</h5>
+      </div>
+      </div>
+      </div>
+      <div className="col-md-3 col-sm-6">
+      <div className="our-stats-box text-center">
+      <div className="our-stat-icon">
+      <span className="fa fa-git-square"></span>
+      </div>
+      <div className="our-stat-info">
+      <span className="stats" data- from ="1" data-to="504" data-speed="5000"
+      data-refresh-interval="50"></span>
+
+      <h5>Editors Supported</h5>
+      </div>
+      </div>
+      </div>
+      <div className="col-md-3 col-sm-6">
+      <div className="our-stats-box text-center">
+      <div className="our-stat-icon">
+      <span className="fa fa-globe"></span>
+      </div>
+      <div className="our-stat-info">
+      <span className="stats" data- from ="1" data-to="359" data-speed="5000"
+      data-refresh-interval="50"></span>
+
+      <h5>Languages Detected</h5>
+      </div>
+      </div>
+      </div>
+      <div className="col-md-3 col-sm-6">
+      <div className="our-stats-box text-center">
+      <div className="our-stat-icon">
+      <span className="fa fa-gears"></span>
+      </div>
+      <div className="our-stat-info">
+      <span className="stats" data- from ="3500" data-to="5000" data-speed="5000"
+      data-refresh-interval="50"></span>
+
+      <h5>Happy Clients</h5>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </section>
+
+      <section className="client-testimonial text-center white" id="six">
+
+      <div className="container">
+      <div className="row">
+      <div className="col-md-12">
+      <h3 className="title">Client <span className="themecolor">Testimonials</span></h3>
+      </div>
+
+      <div className="col-md-8 col-md-offset-2 grey">
+      <div id="testimonial" className="owl-carousel owl-theme">
+      <div className="item">
+      <h5>This book is a treatise on the theory of ethics, very popular during the Renaissance.The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h5>
+      <p><strong>Dean Martin</strong>, CEO Acme Inc.</p>
+      </div>
+
+      <div className="item">
+      <h5>This book is a treatise on the theory of ethics, very popular during the Renaissance.The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h5>
+      <p><strong>Dean Martin</strong>, CEO Acme Inc.</p>
+      </div>
+
+      <div className="item">
+      <h5>This book is a treatise on the theory of ethics, very popular during the Renaissance.The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h5>
+      <p><strong>Dean Martin</strong>, CEO Acme Inc.</p>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      </section>
+
+      <section className="services white-background black" id="seven">
+      <div className="container">
+      <div className="row text-center">
+      <div className="col-md-12">
+      <h3 className="title">We Are <span className="themecolor">Good In</span></h3>
+      <p className="a-slog">Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty</p>
+      </div>
+      </div>
+      <div className="gap"></div>
+
+      <div className="row">
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-diamond"></i>
+      <div className="nim-service-detail">
+      <h4>Strategy Solutions</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-tablet"></i>
+      <div className="nim-service-detail">
+      <h4>Digital Design</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-magic"></i>
+      <div className="nim-service-detail">
+      <h4>SEO</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      <div className="row">
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-rocket"></i>
+      <div className="nim-service-detail">
+      <h4>Graphic Design</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-map-marker"></i>
+      <div className="nim-service-detail">
+      <h4>Analystics</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-paypal"></i>
+      <div className="nim-service-detail">
+      <h4>Dedicated Support</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      <div className="row">
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-bar-chart-o"></i>
+      <div className="nim-service-detail">
+      <h4>Truly Multipurpose</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-delicious"></i>
+      <div className="nim-service-detail">
+      <h4>Unlimited Colors</h4>
+      <p>We put a lot of effort in design, as it’s the most important ingredient of successful website.Sed ut perspiciatis unde omnis iste natus error sit.</p>
+      </div>
+      </div>
+      </div>
+
+      <div className="col-sm-4">
+      <div className="nim-service margin-bottom">
+      <i className="fa fa-pencil-square"></i>
+      <div className="nim-service-detail">
+      <h4>Easy to customize</h4>
+      <p>Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty dolor site amet ne onseceruter adipiscing</p>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      </div>
+      </section>
+
+
+      <section className="sub-form text-center" id="eight">
+      <div className="container">
+      <div className="col-md-12">
+      <h3 className="title">Subscribe to our <span className="themecolor"> News letter</span></h3>
+      <p className="lead">Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty</p>
+      </div>
+      <div className="row">
+      <div className="col-md-3 col-sm-3"></div>
+      <div className="col-md-6 center-block col-sm-6 ">
+      <form id="mc-form">
+      <div className="input-group">
+      <input type ="email" className="form-control" placeholder="Email Address" required id="mc-email" />
+      <span className="input-group-btn">
+      <button type ="submit" className="btn btn-default">SUBSCRIBE <i className="fa fa-envelope"></i> </button>
+      </span> </div>
+      <label for="mc-email" id="mc-notification"></label>
+      </form>
+      </div>
+      </div>
+      </div>
+      </section>
+
+      <footer className="site-footer section-spacing text-center " id="eight">
+
+      <div className="container">
+      <div className="row">
+      <div className="col-md-4">
+      <p className="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      </div>
+      <div className="col-md-4"> <small>&copy; 2015 Nim.All rights reserved.</small></div>
+      <div className="col-md-4">
+
+      <ul className="social">
+      <li><a href="https://twitter.com/" target="_blank"><i className="fa fa-twitter "></i></a></li>
+      <li><a href="https://www.facebook.com/" target="_blank"><i className="fa fa-facebook"></i></a></li>
+      <li><a href="https://www.youtube.com/" target="_blank"><i className="fa fa-youtube-play"></i></a></li>
+      </ul>
+
+      </div>
+      </div>
+      </div>
+      </footer>
+
       </div>
     </div>
   );
