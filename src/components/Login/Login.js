9b8764b52b4c1d2c569b34ea8144be17e6c9d751
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-09 11:38:07 +0200

    Add dummy login functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
+import * as authService from "../../services/authService.js";
+
 const Login = () => {
+    const navigate = useNavigate();
+
+    const onLoginHandler = (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(e.currentTarget);
+        let email = formData.get("email");
+        let password = formData.get("password");
+
+        authService.login(email);
+
+        navigate("/");
+    };
+
     return(
         <section id="login-page" className="login">
-            <form id="login-form" method="POST">
+            <form id="login-form" onSubmit={onLoginHandler} method="POST">
                 <fieldset>
                     <legend>Login Form</legend>
                     <p className="field">
