4c3455189b51f53b1172c25bde3914037645dbb6
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-09 09:57:14 +0200

    Added empty modules
    Details, Edit, Login, Logout, Register

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
                 <h1 className="main-heading-title">We are Creative</h1>
                 <p className="main-heading-text">Et harum quidem rerum facilis est et expedita distinctio.Nam libero tempore,<br />cum soluta nobis est eligendi optio cumque nihil impedit quo facilis</p>
                 <div className="btn-bar">
-                  <a href="#" className="btn btn-custom theme_background_color">Ge Started</a>
+                  <a href="#" className="btn btn-custom theme_background_color">Get Started</a>
                   <a href="#" className="btn btn-custom-outline">Contact Us</a>
                 </div>
               </div>
