dfc04fbdb0af2e7820686344a70ac548b89ab4b4
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-10 19:05:37 +0200

    Change dashboard text

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,8 +5,9 @@ const Dashboard = () => {
           <div className="container">
             <div className="row">
               <div className="main-heading-content col-md-12 col-sm-12 text-center">
-                <h1 className="main-heading-title">We are Creative</h1>
-                <p className="main-heading-text">Et harum quidem rerum facilis est et expedita distinctio.Nam libero tempore,<br />cum soluta nobis est eligendi optio cumque nihil impedit quo facilis</p>
+                <h1 className="main-heading-title">The best deals</h1>
+                <p className="main-heading-text">The best clothing deals are here! Check them out, like, comment and share the latest amazing offers.</p>
+                <p className="main-heading-text">Join our community and help everyone to get a great deal!</p>
                 <div className="btn-bar">
                   <a href="#" className="btn btn-custom theme_background_color">Get Started</a>
                   <a href="#" className="btn btn-custom-outline">Contact Us</a>
