e41f0a9bd55381c849140b357a5a1ef98638b7b3
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-14 12:31:43 +0200

    Edit About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
           <div className="row">
             <div className="col-md-12 text-center black">
               <h3 className="title">ABOUT <span className="themecolor">US</span></h3>
-              <p className="a-slog">Lorem ipsum dolor sit amet ne onsectetuer adipiscing elit.Aenean commodo ligula eget dolor in tashin ty</p>
+              <p className="a-slog">Our mission is to connect people and help them share the best deals they've found with other people. Thus saving your time and wallet for free!</p>
             </div>
           </div>
           <div className="gap">
@@ -15,23 +15,23 @@ const About = () => {
           <div className="row about-box">
             <div className="col-sm-4 text-center">
               <div className="margin-bottom">
-                <i className="fa fa-newspaper-o"></i>
-                <h4>POWER OF FLEXIBILITY</h4>
-                <p className="black">Nulla vitae libero pharetra augue.Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+                <i className="fa fa-users"></i>
+                <h4>COMMUNITY</h4>
+                <p className="black">We bring the community together and help it reach other amazing people.</p>
               </div>
             </div>
             <div className="col-sm-4 about-line text-center">
               <div className="margin-bottom">
-                <i className="fa fa-diamond"></i>
-                <h4>FULLY RESPONSIVE</h4>
-                <p className="black">Nulla vitae libero pharetra augue.Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+                <i className="fa fa-comments"></i>
+                <h4>SHARE YOUR INSIGHTS</h4>
+                <p className="black">We are happy to offer you the opurtunity to share you amazing ideas and findings.</p>
               </div>
             </div>
             <div className="col-sm-4 text-center">
               <div className="margin-bottom">
-                <i className="fa fa-area-chart"></i>
-                <h4>GREAT WEB IDEAS</h4>
-                <p className="black">Nulla vitae libero pharetra augue.Etiam porta malesuada magna mollis euismod consectetur sem urdom tempus porttitor.</p>
+                <i className="fa fa-dollar"></i>
+                <h4>FREE OF CHARGE</h4>
+                <p className="black">We are proud to offer you our services for free as they always have and will be!</p>
               </div>
             </div>
           </div>
