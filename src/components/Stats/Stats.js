b95ff282ba032fea6a88c74131867c46d1771851
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-20 19:00:03 +0200

    Improve Stats design

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,66 +1,72 @@
+import { useEffect, useState } from "react";
+import * as dealService from "../../services/dealService.js";
 import "./Stats.css";
 
 const Stats = () => {
-    return(
-        <section className="our-stats" id="five">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 col-sm-6">
-              <div className="our-stats-box text-center">
-                <div className="our-stat-icon">
-                  <span className="fa fa-ge"></span>
-                </div>
-                <div className="our-stat-info">
-                  <span className="stats" data- from="4763904" data-to="4764504" data-speed="5000"
-                    data-refresh-interval="50"></span>
+  const [deals, setDeals] = useState([]);
 
-                  <h5>Total Downloaded</h5>
-                </div>
+  useEffect(() => {
+    dealService.getAll()
+      .then(result => {
+        setDeals(result);
+      });
+  }, []);
+  return (
+    <section className="our-stats" id="five">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <div className="our-stats-box text-center">
+              <div className="our-stat-icon">
+                <span className="fa fa-newspaper-o"></span>
+              </div>
+              <div className="our-stat-info">
+                <h3>{deals.length}</h3>
+
+                <h5>Total Posts</h5>
               </div>
             </div>
-            <div className="col-md-3 col-sm-6">
-              <div className="our-stats-box text-center">
-                <div className="our-stat-icon">
-                  <span className="fa fa-git-square"></span>
-                </div>
-                <div className="our-stat-info">
-                  <span className="stats" data- from="1" data-to="504" data-speed="5000"
-                    data-refresh-interval="50"></span>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="our-stats-box text-center">
+              <div className="our-stat-icon">
+                <span className="fa fa-child"></span>
+              </div>
+              <div className="our-stat-info">
+                <h3>100+</h3>
 
-                  <h5>Editors Supported</h5>
-                </div>
+                <h5>Visits Per Month</h5>
               </div>
             </div>
-            <div className="col-md-3 col-sm-6">
-              <div className="our-stats-box text-center">
-                <div className="our-stat-icon">
-                  <span className="fa fa-globe"></span>
-                </div>
-                <div className="our-stat-info">
-                  <span className="stats" data- from="1" data-to="359" data-speed="5000"
-                    data-refresh-interval="50"></span>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="our-stats-box text-center">
+              <div className="our-stat-icon">
+                <span className="fa fa-globe"></span>
+              </div>
+              <div className="our-stat-info">
+                <h3>24/7</h3>
 
-                  <h5>Languages Detected</h5>
-                </div>
+                <h5>Access</h5>
               </div>
             </div>
-            <div className="col-md-3 col-sm-6">
-              <div className="our-stats-box text-center">
-                <div className="our-stat-icon">
-                  <span className="fa fa-gears"></span>
-                </div>
-                <div className="our-stat-info">
-                  <span className="stats" data- from="3500" data-to="5000" data-speed="5000"
-                    data-refresh-interval="50"></span>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="our-stats-box text-center">
+              <div className="our-stat-icon">
+                <span className="fa fa-gears"></span>
+              </div>
+              <div className="our-stat-info">
+                <h3>2021</h3>
 
-                  <h5>Happy Clients</h5>
-                </div>
+                <h5>Running Since</h5>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
