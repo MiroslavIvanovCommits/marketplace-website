00fad157cd69cab8a2752b598dac887e44d6aa78
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-20 10:50:16 +0200

    Improve Dashboard design

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Dashboard.css";
 
 const Dashboard = () => {
@@ -8,11 +9,11 @@ const Dashboard = () => {
             <div className="row">
               <div className="main-heading-content col-md-12 col-sm-12 text-center">
                 <h1 className="main-heading-title">The best deals</h1>
-                <p className="main-heading-text">The best clothing deals are here! Check them out, like, comment and share the latest amazing offers.</p>
+                <p className="main-heading-text">The best clothing deals are here! Check them out, like and share the latest amazing offers.</p>
                 <p className="main-heading-text">Join our community and help everyone to get a great deal!</p>
                 <div className="button-bar">
-                  <a href="#" className="button button-custom theme_background_color">Get Started</a>
-                  <a href="#" className="button button-custom-outline">Contact Us</a>
+                  <Link to="/register" className="button button-custom theme_background_color">Sign Up</Link>
+                  <Link to="/about" className="button button-custom-outline">About Us</Link>
                 </div>
               </div>
             </div>
