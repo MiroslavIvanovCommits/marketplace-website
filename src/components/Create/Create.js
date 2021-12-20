caf02c021e6266b0f1c6df3e0355ab127d163910
MiroslavIvanov <miro.marvel@prtonmail.com>
2021-12-20 18:18:45 +0200

    Add data validation with RHF and Yup for Create and Edit

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -2,20 +2,36 @@ import * as dealService from "../../services/dealService.js";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext.js";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import "./Create.css";
+
+const schema = yup.object().shape({
+    name: yup.string().required()
+        .min(8, "Name must be at least 8 symbols long.")
+        .max(60, "Name must be at most 60 symbols long."),
+    description: yup.string().required()
+        .min(30, "Description must be at least 30 symbols long.")
+        .max(2000, "Description must be at most 2000 symbols long."),
+    imageUrl: yup.string().required()
+        .matches(/^https?:\/\//g, "Please enter a valid url."),
+    type: yup.string().required()
+        .matches(/(^clothing$)|(^shoes$)|(^accessories$)/g, "Type must be one of the following: clothing, shoes, accessories")
+});
 
 const Create = () => {
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const onDealCreate = (e) => {
-        e.preventDefault();
-        let formData = new FormData(e.currentTarget);
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onBlur",
+        criteriaMode: "all"
+    });
 
-        let name = formData.get("name");
-        let description = formData.get("description");
-        let imageUrl = formData.get("imageUrl");
-        let type = formData.get("type");
+    const onDealCreate = (data) => {
+        let { name, description, imageUrl, type } = data;
 
         dealService.create({
             name,
@@ -29,43 +45,47 @@ const Create = () => {
     };
     return (
         <section id="create-page" className="create">
-            <form id="create-form" onSubmit={onDealCreate} method="">
-            <div className="row">
-                <fieldset id="create-fieldset">
-                    <label className="legend">Add new Deal</label>
-                    <div className="create-icon">
+            <form id="create-form" onSubmit={handleSubmit((data) => onDealCreate(data))}>
+                <div className="row">
+                    <fieldset id="create-fieldset">
+                        <label className="legend">Add new Deal</label>
+                        <div className="create-icon">
                             <i className="fa fa-pencil-square-o"></i>
                         </div>
-                    <p className="field">
-                        <label htmlFor="name">Name</label>
-                        <span className="input">
-                            <input type="text" name="name" className="create-input" placeholder="Name" />
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="description">Description</label>
-                        <span className="input">
-                            <textarea name="description" className="create-input create-description" placeholder="Description"></textarea>
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="image">Image</label>
-                        <span className="input">
-                            <input type="text" name="imageUrl" className="create-input" placeholder="Image" />
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="type">Type</label>
-                        <span className="create-input">
-                            <select id="type" name="type">
-                                <option value="clothing">Clothing</option>
-                                <option value="shoes">Shoes</option>
-                                <option value="accessories">Accessories</option>
-                            </select>
-                        </span>
-                    </p>
-                    <input className="btn" type="submit" value="Add Deal" />
-                </fieldset>
+                        <p className="field">
+                            <label htmlFor="name">Name</label>
+                            <span className="input">
+                                <input type="text" name="name" className="create-input" placeholder="Name" {...register("name")} />
+                                <p>{errors.name?.message}</p>
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="description">Description</label>
+                            <span className="input">
+                                <textarea name="description" className="create-input create-description" placeholder="Description" {...register("description")}></textarea>
+                                <p>{errors.description?.message}</p>
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="image">Image</label>
+                            <span className="input">
+                                <input type="text" name="imageUrl" className="create-input" placeholder="Image" {...register("imageUrl")} />
+                                <p>{errors.imageUrl?.message}</p>
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="type">Type</label>
+                            <span className="create-input">
+                                <select id="type" name="type" {...register("type")}>
+                                    <option value="clothing">Clothing</option>
+                                    <option value="shoes">Shoes</option>
+                                    <option value="accessories">Accessories</option>
+                                </select>
+                                <p>{errors.type?.message}</p>
+                            </span>
+                        </p>
+                        <input className="btn" type="submit" value="Add Deal" />
+                    </fieldset>
                 </div>
             </form>
         </section>
--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,6 +1,9 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import * as dealService from '../../services/dealService.js';
 import useDealState from '../../hooks/useDealState';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import "./Edit.css";
 
 const types = [
@@ -9,57 +12,76 @@ const types = [
     { value: 'accessories', text: 'Accessories' }
 ];
 
+const schema = yup.object().shape({
+    name: yup.string().required()
+        .min(8, "Name must be at least 8 symbols long.")
+        .max(60, "Name must be at most 60 symbols long."),
+    description: yup.string().required()
+        .min(30, "Description must be at least 30 symbols long.")
+        .max(2000, "Description must be at most 2000 symbols long."),
+    imageUrl: yup.string().required()
+        .matches(/^https?:\/\//g, "Please enter a valid url."),
+    type: yup.string().required()
+        .matches(/(^clothing$)|(^shoes$)|(^accessories$)/g, "Type must be one of the following: clothing, shoes, accessories")
+});
+
 const Edit = () => {
     const { dealId } = useParams();
     const [deal, setDeal] = useDealState(dealId);
     const navigate = useNavigate();
 
-    const dealEditSubmitHandler = (e) => {
-        e.preventDefault();
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onBlur",
+        criteriaMode: "all"
+    });
 
-        let dealData = Object.fromEntries(new FormData(e.currentTarget));
-
-        dealService.update(deal._id, dealData)
+    const dealEditSubmitHandler = (data) => {
+        dealService.update(deal._id, data)
             .then(navigate("/catalog"));
     };
 
     return (
         <section id="edit-page" className="edit">
-            <form id="edit-form" method="POST" onSubmit={dealEditSubmitHandler}>
-            <div className="row">
-                <fieldset id="edit-fieldset">
-                    <label className="legend">Edit my Deal</label>
-                    <div className="create-icon">
+            <form id="edit-form" onSubmit={handleSubmit((data) => dealEditSubmitHandler(data))}>
+                <div className="row">
+                    <fieldset id="edit-fieldset">
+                        <label className="legend">Edit my Deal</label>
+                        <div className="create-icon">
                             <i className="fa fa-pencil"></i>
                         </div>
-                    <p className="field">
-                        <label htmlFor="name">Name</label>
-                        <span className="input">
-                            <input type="text" name="name" className="create-input" defaultValue={deal.name} />
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="description">Description</label>
-                        <span className="edit-input">
-                            <textarea name="description" className="create-input create-description" defaultValue={deal.description} />
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="image">Image</label>
-                        <span className="edit-input">
-                            <input type="text" name="imageUrl" className="create-input" defaultValue={deal.imageUrl} />
-                        </span>
-                    </p>
-                    <p className="field">
-                        <label htmlFor="type">Type</label>
-                        <span className="edit-input">
-                            <select name="type" className="create-input" value={deal.type} onChange={(e) => setDeal(s => ({ ...s, type: e.target.value }))}>
-                                {types.map(x => <option key={x.value} value={x.value}>{x.text}</option>)}
-                            </select>
-                        </span>
-                    </p>
-                    <input className="btn" type="submit" value="Save" />
-                </fieldset>
+                        <p className="field">
+                            <label htmlFor="name">Name</label>
+                            <span className="input">
+                                <input type="text" name="name" className="create-input" defaultValue={deal.name} {...register("name")} />
+                                <p>{errors.name?.message}</p>
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="description">Description</label>
+                            <span className="edit-input">
+                                <textarea name="description" className="create-input create-description" defaultValue={deal.description} {...register("description")} />
+                                <p>{errors.description?.message}</p>
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="image">Image</label>
+                            <span className="edit-input">
+                                <input type="text" name="imageUrl" className="create-input" defaultValue={deal.imageUrl} {...register("imageUrl")} />
+                                <p>{errors.imageUrl?.message}</p>
+                            </span>
+                        </p>
+                        <p className="field">
+                            <label htmlFor="type">Type</label>
+                            <span className="edit-input">
+                                <select name="type" className="create-input" value={deal.type} onChange={(e) => setDeal(s => ({ ...s, type: e.target.value }))} {...register("type")}>
+                                    {types.map(x => <option key={x.value} value={x.value}>{x.text}</option>)}
+                                </select>
+                                <p>{errors.type?.message}</p>
+                            </span>
+                        </p>
+                        <input className="btn" type="submit" value="Save" />
+                    </fieldset>
                 </div>
             </form>
         </section>
